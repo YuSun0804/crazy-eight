36928a9b65a94e0f9427e75d7d72b40fa78d881e
YuSun <yusun@example.com>
2021-01-28 13:24:17 -0500

    update

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -48,6 +48,7 @@ public class GameTest {
                 .addCard(new Card(12, Suit.H)).addCard(new Card(13, Suit.H))
                 .addCard(new Card(10, Suit.C));
         System.out.println("player4's card list is " + player4.getCardList());
+        System.out.println("--------------------");
 
         play(player1, new Card(4, Suit.H));
         play(player2, new Card(4, Suit.S));
@@ -82,7 +83,9 @@ public class GameTest {
 
         score();
         Assert.assertEquals(false, crazyEights.isGameOver());
+        System.out.println("--------------------");
 
+        crazyEights.setTopCard(new Card(10, Suit.D));
         player1.addCard(new Card(1, Suit.D)).addCard(new Card(4, Suit.S))
                 .addCard(new Card(1, Suit.C)).addCard(new Card(4, Suit.H))
                 .addCard(new Card(5, Suit.D));
@@ -102,31 +105,37 @@ public class GameTest {
                 .addCard(new Card(4, Suit.C)).addCard(new Card(5, Suit.S))
                 .addCard(new Card(8, Suit.D));
         System.out.println("player4's card list is " + player4.getCardList());
+        System.out.println("--------------------");
 
         play(player1, new Card(1, Suit.D));
         play(player2, new Card(2, Suit.D));
         play(player3, new Card(3, Suit.D));
         play(player4, new Card(4, Suit.D));
+        System.out.println("--------------------");
 
         play(player1, new Card(4, Suit.S));
         play(player2, new Card(3, Suit.S));
         play(player3, new Card(2, Suit.S));
         play(player4, new Card(1, Suit.S));
+        System.out.println("--------------------");
 
         play(player1, new Card(1, Suit.C));
         play(player2, new Card(2, Suit.C));
         play(player3, new Card(3, Suit.C));
         play(player4, new Card(4, Suit.C));
+        System.out.println("--------------------");
 
         play(player1, new Card(4, Suit.H));
         play(player2, new Card(3, Suit.H));
         play(player3, new Card(2, Suit.H));
         draw(player4, new Card(13, Suit.S)).draw(player4, new Card(12, Suit.S)).draw(player4, new Card(1, Suit.H));
         play(player4, new Card(1, Suit.H));
+        System.out.println("--------------------");
 
         draw(player1, new Card(6, Suit.D)).draw(player1, new Card(7, Suit.D)).draw(player1, new Card(9, Suit.D));
         draw(player2, new Card(6, Suit.S)).draw(player2, new Card(7, Suit.S)).draw(player2, new Card(9, Suit.S));
         play(player3, new Card(5, Suit.H));
+        System.out.println("--------------------");
 
         score();
 
@@ -137,7 +146,7 @@ public class GameTest {
 
     private void score() {
         for (Map.Entry<String, Player> entry : crazyEights.getPlayers().entrySet()) {
-            System.out.println(entry.getValue().getCardList());
+            System.out.println("Player" + entry.getKey()+"'s card list is " + entry.getValue().getCardList());
         }
         Map<String, Integer> scorePad = crazyEights.calculateScore();
         for (Map.Entry<String, Integer> entry : scorePad.entrySet()) {
@@ -149,8 +158,6 @@ public class GameTest {
     private void play(Player player, Card card) {
         System.out.println("Player" + player.getId() + " play " + card);
         player.playCard(card, true);
-        System.out.println(player.getCardList());
-        System.out.println(crazyEights.getTopCard());
     }
 
     private GameTest draw(Player player, Card card) {
